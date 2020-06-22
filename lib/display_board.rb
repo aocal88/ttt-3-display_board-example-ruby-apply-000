9e8a05515a8d82b25f7e2a7248414c60ddf97263
HAN O <a.ocal88@gmail.com>
2020-06-22 21:28:22 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,19 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+
+def say_something_x_times(phrase, x)
+  x.times do
+    puts phrase
+  end
+end
+
+say_something_x_times("Sarina", 10)
+
+def say_hi_x_times(phrase, x)
+  x.times do
+    puts phrase
+  end
+ end
+ 
+ say_hi_x_times("Carpet", 100000)
+ 
+    
